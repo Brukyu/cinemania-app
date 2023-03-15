d55324e09620b004eec712438f14d933255923fc
brukyu <bbrunoigorr@gmail.com>
2023-03-15 14:14:08 -0300

    modified App.tsx in cinemania

--- App.tsx
+++ App.tsx
@@ -1,11 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import { Home } from './src/screens/home';
+import { Home } from './src/screens/Home';
 
 export default function App() {
   return (
-    <View>
+    <View style={styles.container}>
       <Home/>
       <StatusBar style="auto" />
     </View>
@@ -15,8 +15,6 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#242A32",
   },
 });
