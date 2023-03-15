dc57d36cf5619c08c329770931902ea57d3b18b1
brukyu <bbrunoigorr@gmail.com>
2023-03-15 14:14:42 -0300

    modified index.tsx in home

--- src/screens/home/index.tsx
+++ src/screens/home/index.tsx
@@ -1,15 +1,15 @@
 import { View, Text, TextInput } from "react-native"
-import { styles } from "./styles"
-
+import { styles } from "./styles";
+import {MagnifyingGlass} from "phosphor-react-native";
 
 export function Home() {
     return( 
-    <View style={styles.container}>
-        <Text style={styles.headerText}>O que voce quer assistir hoje?</Text>
-        <View style={styles.containerInput}>
-            <TextInput placeholder="Buscar"/>
+        <View style={styles.container}>
+            <Text style={styles.headerText}>O que voce quer assistir hoje?</Text>
+            <View style={styles.containerInput}>
+                <TextInput placeholder="Buscar" style={styles.input}/>
+                <MagnifyingGlass color = "#FFf" size={25} weight="light"/>
+            </View>
         </View>
-        
-    </View>
     )
 }
