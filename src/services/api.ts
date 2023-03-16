4ee8e62bed95e18972a737b62713f5ee40a74c4c
brukyu <bbrunoigorr@gmail.com>
2023-03-16 15:56:44 -0300

    modified api.ts in services

--- src/services/api.ts
+++ src/services/api.ts
@@ -5,6 +5,6 @@ baseURL: "https://api.themoviedb.org/3",
 params: {
     api_key : "0ad07647d444e15d8f05eb3343fb0d16",
     language: "pt-BR",
-    include_adult: true,
+    include_adult: false,
 },
 });
