475ac1445c1bcc2f63e665431153b86ed9772569
brukyu <bbrunoigorr@gmail.com>
2023-03-16 08:53:56 -0300

    modified index.tsx in home

--- src/screens/home/index.tsx
+++ src/screens/home/index.tsx
@@ -1,14 +1,47 @@
-import { View, Text, TextInput } from "react-native"
+import { View, Text, TextInput, FlatList } from "react-native"
 import { styles } from "./styles";
+import { useEffect, useState } from "react";
 import {MagnifyingGlass} from "phosphor-react-native";
+import { api } from "../../services/api";
+import { CardMovies } from "../../components/CardMovies";
+
+interface Movie {
+    id: number;
+    title: string;
+    poster_path: string;
+    overview: string;
+}
 
 export function Home() {
+    const [discoveryMovies, setDiscoveryMovies] = useState<Movie[]>([])
+
+    useEffect(() => {
+        loadMoreData();
+    }, []);
+
+    const loadMoreData = async () => {
+        const response = await api.get("/movie/popular");
+        setDiscoveryMovies(response.data.results);
+        console.log(JSON.stringify(response.data, null, 2))
+    }
     return( 
         <View style={styles.container}>
             <Text style={styles.headerText}>O que voce quer assistir hoje?</Text>
             <View style={styles.containerInput}>
-                <TextInput placeholder="Buscar" style={styles.input}/>
+                <TextInput placeholderTextColor="#FFF" placeholder="Buscar" style={styles.input}/>
                 <MagnifyingGlass color = "#FFf" size={25} weight="light"/>
+            </View>
+            <View>
+                <FlatList
+                  data= {discoveryMovies}
+                  numColumns={3}
+                  renderItem={(item) => <CardMovies data={item.item}/>}
+                  showsVerticalScrollIndicator={false}
+                  contentContainerStyle={{
+                    padding:35,
+                    paddingBottom: 100,
+                  }}
+                />
             </View>
         </View>
     )
